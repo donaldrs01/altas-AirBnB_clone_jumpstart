eba38324c56756ebc06d8ba747c6deb8797d9d08
donaldrs01 <ryan.s.donaldson@gmail.com>
2024-03-11 15:03:14 -0500

    beginning of task 7 - updated storage engines, working on state update

--- models/state.py
+++ models/state.py
@@ -19,9 +19,9 @@ class State(BaseModel, Base):
     else:
         name = ""
         cites = []
-    
-    if getenv("HBNB_TYPE_STORAGE") != 'db':
-        @property
-        def cities(self):
-            """Returns list of City objects from FileStorage linked to state"""
-            city_objects = [city for city in storage.all(City).values()
+   
+#   if getenv("HBNB_TYPE_STORAGE") != 'db':
+#       @property
+#           def cities(self):
+#               """Returns list of City objects from FileStorage linked to state"""
+#               city_objects = [city for city in storage.all(City).values()
