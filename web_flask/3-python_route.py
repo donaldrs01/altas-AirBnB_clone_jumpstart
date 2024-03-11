fec0994f97c9a26338a9ad0f64ca5b6f9063f6cf
donaldrs01 <ryan.s.donaldson@gmail.com>
2024-03-11 13:35:57 -0500

    Python route addition

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+Script for starting a Flask web application
+Contains previous 3 methods + Python method
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def text_display(text):
+    text = text.replace('_', ' ')
+    return f'C {text}'
+
+
+@app.route('/python/<text>', strict_slashes=False)
+def python_display(text-"is cool"):
+    text = text.replace('_', ' ')
+    return f'Python {text}'
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
