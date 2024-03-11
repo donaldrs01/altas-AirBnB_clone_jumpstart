75a4049fb792d0362fb7d1605eb1cbfec6351e7a
donaldrs01 <ryan.s.donaldson@gmail.com>
2024-03-11 14:09:32 -0500

    don't include comments in HTML file

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Script that starts a Flask web application. 
+Script that starts a Flask web application.
 Contains previous routes as well as a route that renders
 HTML template containing H1 tag with n=number.
 """
