257f3f7a4c065a6f2b5ddf84f2f18ca72de1e9ad
donaldrs01 <ryan.s.donaldson@gmail.com>
2024-03-12 15:47:48 -0500

    trying to fix circular import issue

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -9,13 +9,6 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.teardown_appcontext
-def teardown(exception):
-    """ Method to close current SQLAlchemy session"""
-    from models import storage
-    storage.close()
-
-
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     from models import storage
@@ -23,6 +16,12 @@ def states_list():
     states = storage.all('State')
     return render_template('7-states_list.html', states=states)
 
+
+@app.teardown_appcontext
+def teardown(exception):
+    """ Method to close current SQLAlchemy session"""
+    from models import storage
+    storage.close()
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
